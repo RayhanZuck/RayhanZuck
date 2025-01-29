6375429f6d37c679a0d8e7b454cea62925f96462
Rayhan Zuckerberg <76443273+RayhanZuck@users.noreply.github.com>
2025-01-29 10:47:32 +0700

    Create heroml.js

---
+++ json/heroml.js
@@ -0,0 +1,394 @@
+[
+  {
+    "soal": "Aamon"
+  },
+  {
+    "soal": "Assassin"
+  },
+  {
+    "soal": "Jungler"
+  },
+  {
+    "soal": "Akai"
+  },
+  {
+    "soal": "Tank"
+  },
+  {
+    "soal": "Aldous"
+  },
+  {
+    "soal": "Fighter"
+  },
+  {
+    "soal": "Alice"
+  },
+  {
+    "soal": "Alpha"
+  },
+  {
+    "soal": "Alucard"
+  },
+  {
+    "soal": "Angela"
+  },
+  {
+    "soal": "Support"
+  },
+  {
+    "soal": "Roamer"
+  },
+  {
+    "soal": "Argus"
+  },
+  {
+    "soal": "EXP Laner"
+  },
+  {
+    "soal": "Arlott"
+  },
+  {
+    "soal": "Atlas"
+  },
+  {
+    "soal": "Aulus"
+  },
+  {
+    "soal": "Aurora"
+  },
+  {
+    "soal": "Mage"
+  },
+  {
+    "soal": "Badang"
+  },
+  {
+    "soal": "Balmond"
+  },
+  {
+    "soal": "Bane"
+  },
+  {
+    "soal": "Barats"
+  },
+  {
+    "soal": "Baxia"
+  },
+  {
+    "soal": "Beatrix"
+  },
+  {
+    "soal": "Marksman"
+  },
+  {
+    "soal": "Gold Laner"
+  },
+  {
+    "soal": "Belerick"
+  },
+  {
+    "soal": "Benedetta"
+  },
+  {
+    "soal": "Brody"
+  },
+  {
+    "soal": "Bruno"
+  },
+  {
+    "soal": "Carmilla"
+  },
+  {
+    "soal": "Caecilion"
+  },
+  {
+    "soal": "Mid Laner"
+  },
+  {
+    "soal": "Chou"
+  },
+  {
+    "soal": "Figter"
+  },
+  {
+    "soal": "Cici"
+  },
+  {
+    "soal": "Claude"
+  },
+  {
+    "soal": "Clint"
+  },
+  {
+    "soal": "Cyclops"
+  },
+  {
+    "soal": "Diggie"
+  },
+  {
+    "soal": "Dyrroth"
+  },
+  {
+    "soal": "Edith"
+  },
+  {
+    "soal": "Esmeralda"
+  },
+  {
+    "soal": "Estes"
+  },
+  {
+    "soal": "Eudora"
+  },
+  {
+    "soal": "Fanny"
+  },
+  {
+    "soal": "Faramis"
+  },
+  {
+    "soal": "Floryn"
+  },
+  {
+    "soal": "Franco"
+  },
+  {
+    "soal": "Fredrinn"
+  },
+  {
+    "soal": "Freya"
+  },
+  {
+    "soal": "Gatotkaca"
+  },
+  {
+    "soal": "Gloo"
+  },
+  {
+    "soal": "Gord"
+  },
+  {
+    "soal": "Granger"
+  },
+  {
+    "soal": "Grock"
+  },
+  {
+    "soal": "Guinevere"
+  },
+  {
+    "soal": "Gusion"
+  },
+  {
+    "soal": "Hanabi"
+  },
+  {
+    "soal": "Hanzo"
+  },
+  {
+    "soal": "Harith"
+  },
+  {
+    "soal": "Harley"
+  },
+  {
+    "soal": "Hayabusa"
+  },
+  {
+    "soal": "Helcurt"
+  },
+  {
+    "soal": "Hilda"
+  },
+  {
+    "soal": "Hylos"
+  },
+  {
+    "soal": "Irithel"
+  },
+  {
+    "soal": "Ixia"
+  },
+  {
+    "soal": "Jawhead"
+  },
+  {
+    "soal": "Johnson"
+  },
+  {
+    "soal": "Joy"
+  },
+  {
+    "soal": "Asassin"
+  },
+  {
+    "soal": "Julian"
+  },
+  {
+    "soal": "Kadita"
+  },
+  {
+    "soal": "Kagura"
+  },
+  {
+    "soal": "Kaja"
+  },
+  {
+    "soal": "Karina"
+  },
+  {
+    "soal": "Karrie"
+  },
+  {
+    "soal": "Khaleed"
+  },
+  {
+    "soal": "Khufra"
+  },
+  {
+    "soal": "Kimmy"
+  },
+  {
+    "soal": "Lancelot"
+  },
+  {
+    "soal": "Layla"
+  },
+  {
+    "soal": "Leomord"
+  },
+  {
+    "soal": "Lesley"
+  },
+  {
+    "soal": "Ling"
+  },
+  {
+    "soal": "Lolita"
+  },
+  {
+    "soal": "Lunox"
+  },
+  {
+    "soal": "Luo Yi"
+  },
+  {
+    "soal": "Lylia"
+  },
+  {
+    "soal": "Martis"
+  },
+  {
+    "soal": "Masha"
+  },
+  {
+    "soal": "Mathilda"
+  },
+  {
+    "soal": "Melissa"
+  },
+  {
+    "soal": "Minotaur"
+  },
+  {
+    "soal": "Minsitthar"
+  },
+  {
+    "soal": "Miya"
+  },
+  {
+    "soal": "Moskov"
+  },
+  {
+    "soal": "Nana"
+  },
+  {
+    "soal": "Natalia"
+  },
+  {
+    "soal": "Natan"
+  },
+  {
+    "soal": "Novaria"
+  },
+  {
+    "soal": "Odette"
+  },
+  {
+    "soal": "Paquito"
+  },
+  {
+    "soal": "Pharsa"
+  },
+  {
+    "soal": "Phoveus"
+  },
+  {
+    "soal": "Popol and Kupa"
+  },
+  {
+    "soal": "Rafaela"
+  },
+  {
+    "soal": "Roger"
+  },
+  {
+    "soal": "Ruby"
+  },
+  {
+    "soal": "Saber"
+  },
+  {
+    "soal": "Selena"
+  },
+  {
+    "soal": "Silvanna"
+  },
+  {
+    "soal": "Sun"
+  },
+  {
+    "soal": "Terizla"
+  },
+  {
+    "soal": "Thamuz"
+  },
+  {
+    "soal": "Tigreal"
+  },
+  {
+    "soal": "Uranus"
+  },
+  {
+    "soal": "Vale"
+  },
+  {
+    "soal": "Valentina"
+  },
+  {
+    "soal": "Valir"
+  },
+  {
+    "soal": "Vexana"
+  },
+  {
+    "soal": "Wanwan"
+  },
+  {
+    "soal": "Xavier"
+  },
+  {
+    "soal": "Yin"
+  },
+  {
+    "soal": "Yu Zhong"
+  },
+  {
+    "soal": "Yve"
+  },
+  {
+    "soal": "Zhask"
+  },
+  {
+    "soal": "Zilong"
+]
